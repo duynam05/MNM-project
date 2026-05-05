82723a6be96891e116987547d25906a0cc61a3cf
Trịnh Duy Nam <duynam05112005@gmail.com>
2026-05-05 10:30:16 +0700

    update dockerfile and deloy be

---
+++ backend/laravel-api/docker/start.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+set -e
+
+mkdir -p storage/framework/cache storage/framework/sessions storage/framework/views bootstrap/cache
+
+php artisan package:discover --ansi
+php artisan storage:link || true
+php artisan migrate --force
+
+exec php artisan serve --host=0.0.0.0 --port="${PORT:-10000}"
